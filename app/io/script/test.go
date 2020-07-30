2e2df193280371b2a9f11296cffba363b01055ad
Dmitry Soloma <flyingmeatsoul@gmail.com>
2020-07-30 13:54:52 +0300

    Fix crontab task run commend

--- app/io/script/test.go
+++ app/io/script/test.go
@@ -4,10 +4,11 @@ package script
 import (
 	"github.com/dimonrus/gocli"
 	"gost/app/base"
+	"os"
 )
 
 func init() {
 	base.App.GetScripts()["test"] = func(args gocli.Arguments) {
-		base.App.GetLogger().Infoln("cron is works")
+		base.App.GetLogger().Infoln("cron is works, ENV=", os.Getenv("ENV"))
 	}
 }
