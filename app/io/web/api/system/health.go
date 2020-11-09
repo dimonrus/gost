e45ee3b473909cd7eba7c071d37ff9575e709c4f
d.soloma <d.soloma@id-netwotk.ru>
2020-11-09 11:59:00 +0300

    Update system routes

--- app/io/web/api/system/health.go
+++ app/io/web/api/system/health.go
@@ -5,16 +5,48 @@ import (
 	"github.com/dimonrus/gorest"
 	"github.com/dimonrus/porterr"
 	"gost/app/base"
+	"io/ioutil"
 	"net/http"
 	"runtime"
 )
 
-// Health Check
+// swagger:route GET /system/health system HealthCheckHandler
+//
+// System. Health check
+//
+// This will show information about health of service.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: https
+//
+//     Responses:
+//       200: ResponseMessage
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	gorest.Send(w, gorest.NewOkJsonResponse("Alive", nil, nil))
 }
 
-// Ping db
+// swagger:route GET /system/health-db system HealthDB
+//
+// System. Health check for db
+//
+// This will show information about health of db.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: https
+//
+//     Responses:
+//       200: ResponseMessage
+//       500: ResponseError
 func HealthDB(w http.ResponseWriter, r *http.Request) {
 	err := base.App.GetDB().Ping()
 	if err != nil {
@@ -25,7 +57,22 @@ func HealthDB(w http.ResponseWriter, r *http.Request) {
 	gorest.Send(w, gorest.NewOkJsonResponse("Database is alive", nil, nil))
 }
 
-// Memory Usage
+// swagger:route GET /system/memory system MemoryUsage
+//
+// System. Memory usage
+//
+// This will show information about memory of service.
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: https
+//
+//     Responses:
+//       200: ResponseMemoryUsage
 func MemoryUsage(w http.ResponseWriter, r *http.Request) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
@@ -35,4 +82,19 @@ func MemoryUsage(w http.ResponseWriter, r *http.Request) {
 	report["system"] = fmt.Sprintf("%v KB", m.Sys/1024)
 	report["garbage_collectors"] = fmt.Sprintf("%v", m.NumGC)
 	gorest.Send(w, gorest.NewOkJsonResponse("Memory usage", report, nil))
+}
+
+// Swagger
+func Swagger(w http.ResponseWriter, r *http.Request) {
+	var e porterr.IError
+	if file, err := ioutil.ReadFile(base.App.GetAbsolutePath("resource/swagger.json")); err == nil {
+		_, err := w.Write(file)
+		if err == nil {
+			return
+		}
+		e = porterr.New(porterr.PortErrorIO, "file cannot be write "+err.Error())
+	} else {
+		e = porterr.New(porterr.PortErrorIO, "file cannot be read "+err.Error())
+	}
+	gorest.Send(w, gorest.NewErrorJsonResponse(e))
 }
