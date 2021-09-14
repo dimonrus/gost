8d2f4aa19ea4f9ed25c5ff486e708f32087b5a62
Dmitry Soloma <flyingmeatsoul@gmail.com>
2021-09-14 09:50:55 +0300

    Fix rabbit queue name

--- app/io/consumer/test.go
+++ app/io/consumer/test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	base.App.GetRabbit().GetRegistry()["test"] = &gorabbit.Consumer{Queue: "golkp.test", Server: "local", Count: 1, Callback: func(d amqp.Delivery) {
+	base.App.GetRabbit().GetRegistry()["test"] = &gorabbit.Consumer{Queue: "gost.test", Server: "local", Count: 1, Callback: func(d amqp.Delivery) {
 		// show the message
 		base.App.GetLogger().Infof("%s", d.Body)
 	}}
